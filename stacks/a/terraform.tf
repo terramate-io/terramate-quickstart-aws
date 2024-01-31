fdbe625c945acc6d6d042c87f88248fd527c47d0
Soren Martius <soeren.martius@gmail.com>
2024-01-31 13:45:55 +0100

    feat: add a first stack

---
+++ stacks/a/terraform.tf
@@ -0,0 +1,16 @@
+// TERRAMATE: GENERATED AUTOMATICALLY DO NOT EDIT
+
+terraform {
+  required_version = "1.6.6"
+}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.31"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
