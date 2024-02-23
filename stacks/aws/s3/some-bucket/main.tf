4a2124cebcc68f77326e257ba4cae05bee58dcf6
Soren Martius <soeren.martius@gmail.com>
2024-02-23 08:43:30 +0100

    feat: destroy test resources

--- stacks/aws/s3/some-bucket/main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "some-terramate-example-bucket" // Replace with your desired bucket name
-  acl    = "private"                       // Replace with the desired ACL (Access Control List)
-}
