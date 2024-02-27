af5e54fe4010f0aaedccabf9585974debb84dde5
Soren Martius <soeren.martius@gmail.com>
2024-02-27 18:07:35 +0100

    chore: remove demo resources

--- stacks/us-east-1/s3/another-s3-test/main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "private_bucket" {
-  bucket = "some-terramate-private-bucket"
-  acl    = "private"
-}
--- stacks/us-east-1/s3/s3-test/main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "terramate-s3-test"
-  acl    = "private"
-}
