4897ac0b06df1ff8fa265fd371bb5abb15be561b
Soren Martius <soeren.martius@gmail.com>
2024-02-28 21:27:34 +0100

    chore: cleanup stacks

--- stacks/us-east-1/s3/a-s3-bucket/main.tf
+++
@@ -1,5 +0,0 @@
-// Define the S3 bucket resource
-resource "aws_s3_bucket" "bucket" {
-  bucket = "terraform-demo-bucket-one"
-  acl    = "private"
-}
--- stacks/us-east-1/s3/another-s3-bucket/main.tf
+++
@@ -1,6 +0,0 @@
-// Define the S3 bucket resource
-resource "aws_s3_bucket" "bucket" {
-  bucket = "terraform-demo-bucket-two"
-  acl    = "private"
-}
-
