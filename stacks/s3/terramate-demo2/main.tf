0669cb44c63a2c1afedf471594b97236202fd351
Soren Martius <soeren.martius@gmail.com>
2024-03-06 17:14:04 +0100

    feat: remove demo stacks

--- stacks/s3/terramate-demo2/main.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "another-terramate-s3-demo-bucket"
-  acl    = "private"
-}
-
--- stacks/s3/terramate-demo3/main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "terramate-demo3" {
-  bucket = "terramate-demo3"
-  acl    = "private"
-}
