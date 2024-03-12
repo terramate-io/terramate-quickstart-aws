873fb2278856a8edc491e1b008412b3ba7d63028
Marius Tolzmann <marius@mineiros.io>
2024-03-12 21:34:26 +0100

    fixup! test: add more stacks

--- path/to/stack/b/_backend.tf
+++ path/to/stack/b/_backend.tf
@@ -5,7 +5,7 @@ terraform {
     bucket         = "terramate-example-terraform-state-backend"
     dynamodb_table = "terraform-lock"
     encrypt        = true
-    key            = "terraform/stacks/by-id/2829c4f6-6b3f-4760-84b8-71353a3c9d50/terraform.tfstate"
+    key            = "terraform/stacks/by-id/ABCDc4f6-6b3f-4760-84b8-71353a3c9d50/terraform.tfstate"
     region         = "us-east-1"
   }
 }
