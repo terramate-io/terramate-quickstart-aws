3d65064f1816c2730affdaea7315bf74df7e095f
Zied Elouaer <zied.elouaer@mineiros.io>
2025-03-28 21:54:19 +0100

    chore: redeploy eks clusters

--- stacks/terraform/envs/stg/eks/main.tf
+++ stacks/terraform/envs/stg/eks/main.tf
@@ -1,2 +1,21 @@
 // TERRAMATE: GENERATED AUTOMATICALLY DO NOT EDIT
 
+module "eks" {
+  cluster_compute_config = {
+    enabled = true
+    node_pools = [
+      "general-purpose",
+    ]
+  }
+  cluster_endpoint_public_access           = true
+  cluster_name                             = "cluster-stg"
+  cluster_version                          = "1.32"
+  enable_cluster_creator_admin_permissions = true
+  source                                   = "terraform-aws-modules/eks/aws"
+  subnet_ids                               = local.subnet_ids
+  tags = {
+    Environment = "stg"
+  }
+  version = "20.34.0"
+  vpc_id  = local.vpc_id
+}
