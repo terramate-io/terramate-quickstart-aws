3937b816a962af97866d8bf03896d0e5aef5d703
Zied Elouaer <zied.elouaer@mineiros.io>
2025-03-28 21:34:53 +0100

    feat: update clusters and enable auto-mode compute

--- stacks/terraform/envs/prd/eks/main.tf
+++ stacks/terraform/envs/prd/eks/main.tf
@@ -1,11 +1,18 @@
 // TERRAMATE: GENERATED AUTOMATICALLY DO NOT EDIT
 
 module "eks" {
-  cluster_endpoint_public_access = true
-  cluster_name                   = "cluster-prd"
-  cluster_version                = "1.32"
-  source                         = "terraform-aws-modules/eks/aws"
-  subnet_ids                     = local.subnet_ids
+  cluster_compute_config = {
+    enabled = true
+    node_pools = [
+      "general-purpose",
+    ]
+  }
+  cluster_endpoint_public_access           = true
+  cluster_name                             = "cluster-prd"
+  cluster_version                          = "1.32"
+  enable_cluster_creator_admin_permissions = true
+  source                                   = "terraform-aws-modules/eks/aws"
+  subnet_ids                               = local.subnet_ids
   tags = {
     Environment = "prd"
   }
