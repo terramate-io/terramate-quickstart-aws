d1e3e8a651777d7452b112151f7b2fa63ccfb924
Soren Martius <soeren.martius@gmail.com>
2025-04-03 11:08:32 +0100

    fix

--- stacks/terraform/envs/stg/null-environment/terraform.tf
+++ stacks/terraform/envs/stg/null-environment/terraform.tf
@@ -9,15 +9,8 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.89"
     }
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.89"
-    }
   }
 }
 provider "aws" {
-  region = "us-east-1"
-}
-provider "google" {
   region = "us-east-1"
 }
